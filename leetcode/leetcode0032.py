f3170b3a3c5368718a86042a6916ccf2ffb04934
qianlong <1339852932@qq.com>
2024-01-22 10:43:19 +0800

    24-01-22

---
+++ leetcode/leetcode0032.py
@@ -0,0 +1,24 @@
+# 力扣-32-最长有效括号
+# 动态规划
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        if n == 0:
+            return 0
+        dp = [0] * n
+        res = 0
+        for i in range(n):
+            if i > 0 and s[i] == ")":
+                if s[i - 1] == "(":
+                    dp[i] = dp[i - 2] + 2
+                elif s[i - 1] == ")" and i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == "(":
+                    dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2]
+                res = max(res, dp[i])
+        return res
+
+
+if __name__ == '__main__':
+    s = input()
+    print(Solution().longestValidParentheses(s))
--- leetcode/leetcode0416.py
+++ leetcode/leetcode0416.py
@@ -1,4 +1,4 @@
-### 力扣-416-分割等和子集 ###
+# 力扣-416-分割等和子集
 # 动态规划-01背包问题
 
 
@@ -12,12 +12,12 @@ class Solution:
         target = sum(nums) // 2
         dp = [0] * (target + 1)
         for num in nums:
-            for j in range(target, num-1, -1):
+            for j in range(target, num - 1, -1):
                 dp[j] = max(dp[j], dp[j-num] + num)
         return dp[-1] == target
 
 
 if __name__ == '__main__':
-    nums = list(map(int, input().split()))
+    nums = [int(i) for i in input().split()]
     print(Solution().canPartition(nums))
     
