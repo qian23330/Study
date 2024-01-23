886afe0a9f47fe2c187f20386fe99692321ec95b
qianlong <1339852932@qq.com>
2024-01-23 10:29:52 +0800

    24-01-23

---
+++ leetcode/leetcode0005.py
@@ -0,0 +1,46 @@
+# 力扣-5-最长回文子串
+# 动态规划
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n < 2:
+            return s
+
+        max_len = 1
+        begin = 0
+        # dp[i][j] 表示 s[i..j] 是否是回文串
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+
+        # 递推开始
+        # 先枚举子串长度
+        for L in range(2, n + 1):
+            # 枚举左边界，左边界的上限设置可以宽松一些
+            for i in range(n):
+                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                j = L + i - 1
+                # 如果右边界越界，就可以退出当前循环
+                if j >= n:
+                    break
+
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+
+                # 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+        return s[begin:begin + max_len]
+
+
+if __name__ == '__main__':
+    s = input()
+    print(Solution().longestPalindrome(s))
---
+++ leetcode/leetcode0064.py
@@ -0,0 +1,22 @@
+# 力扣-62-最小路径和
+# 动态规划
+
+
+from typing import List
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for i in range(m):
+            for j in range(n):
+                dp[i][j] = min(dp[i][j], dp[i-1][j] + grid[i][j], dp[i][j-1] + grid[i][j])
+        return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    grid = [[int(i) for i in line.split(",")] for line in input().split()]
+    print(Solution().minPathSum(grid))
