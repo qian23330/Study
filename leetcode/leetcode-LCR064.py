c19cf833115f87653e78913b3c47f5873a45e744
qianlong <1339852932@qq.com>
2024-02-17 22:36:51 +0800

    24-02-17

---
+++ leetcode/leetcode-LCR064.py
@@ -0,0 +1,32 @@
+# 力扣-LCR064-实现一个魔法字典
+# 数组
+
+
+from typing import List
+
+
+class MagicDictionary:
+
+    def __init__(self):
+        self.words = list()
+
+    def buildDict(self, dictionary: List[str]) -> None:
+        self.words = dictionary
+
+    def search(self, searchWord: str) -> bool:
+        for word in self.words:
+            if len(word) != len(searchWord):
+                continue
+
+            diff = 0
+            for chx, chy in zip(word, searchWord):
+                if chx != chy:
+                    diff += 1
+                    if diff > 1:
+                        break
+
+            if diff == 1:
+                return True
+
+        return False
+
