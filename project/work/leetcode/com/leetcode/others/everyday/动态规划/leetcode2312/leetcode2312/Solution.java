f2fb15cf099bde83707b4d7d243e141834731136
qianlong <1339852932@qq.com>
2024-03-15 12:55:21 +0800

    24-03-15

---
+++ project/work/leetcode/com/leetcode/others/everyday/动态规划/leetcode2312/leetcode2312/Solution.java
@@ -0,0 +1,49 @@
+package com.leetcode.others.everyday.动态规划.leetcode2312.leetcode2312;
+
+/*
+力扣-2312-卖木头块
+每日一题-24.03.15-动态规划
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+class Solution {
+    public long sellingWood(int m, int n, int[][] prices) {
+        long[][] dp = new long[m + 1][n + 1];  // dp[i][j] 为切割高i宽j的木块获得的最大金钱数
+        for (int[] price : prices) {
+            dp[price[0]][price[1]] = price[2];
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= j / 2; k++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[i][j - k]);  // 垂直切割
+                }
+                for (int k = 1; k <= i / 2; k++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[k][j] + dp[i - k][j]);  // 水平切割
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        List<int[]> list = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isBlank()) break;
+            int[] nums = Arrays.stream(line.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            list.add(nums);
+        }
+        int[][] prices = list.toArray(new int[0][]);
+        System.out.println(new Solution().sellingWood(m, n, prices));
+    }
+}
