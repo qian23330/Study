2a7886adae8f5123edb1b59a3a31ed03ac915837
qianlong <1339852932@qq.com>
2024-07-27 21:25:48 +0800

    Revert "24-07-27"
    This reverts commit 9ab05b6ad339e61df21a9bb2f50d18a37abb645d.

---
+++ leetcode/java/com/exam/huawei/round1/Main01.java
@@ -0,0 +1,84 @@
+package com.exam.huawei.round1;
+
+import java.util.*;
+
+/**
+ * 1.云服务计费
+ * 编写一个程序为某云服务计算客户话单，输入为某云服务的计费日志和各种计费因子的计费单价的列表，计费日志内容包含时间戳、客户标识、计费因子、
+ * 计费时长4个字段。日志中如果同一客户同一计费因子在相同时间戳上报多次话单只能计费一次，选先上报的日志计费。计算每个客户的话单总费用。
+ * 解答要求
+ * 时间限制: C/C++ 1000ms,其他语言: 2000ms内存限制: C/C++ 256MB,其他语言: 512MB
+ * 输入
+ * 第1行表示计费日志的条数n，是一个正整数，范围是1<=n<=1000
+ * 第2到n+1行表示云服务的计费日志，共4列，第1列表示时间戳(是一个数字字符串，长度为10) 、第2列表示客户标识(是一个字符串，长度为1-16)，
+ * 第3列表示计费因子 (是一个字符串，长度为1-16，计费因子查不到时认为计费因子单价是0），第四列表示计费时长时长（范围为0-100，
+ * 当计费时长不在范围内要认为是计费日志有问题，当成计费为0处理)，这4个字段使用逗号分隔
+ * 第n+2行表示计费因子的数量m，m是一个正整数，范围是1<=m<=100
+ * 第n+3到n+3+m行表示各种计费因子的计费单价的列表，该表有2列,第1列表示计费因子 (是一个字符串，长度为1-16)，第2列表示单价(是一个正整数，
+ * 范围为1~100)，这2个字段使用逗号分隔
+ * 输出
+ * 每个客户的话单总费用,共2列，第1列表示客户名，第2列表示话单费用，2列用逗号分割，输出按客户标识字典序升序排序
+ * 样例
+ * 输入
+ * 5
+ * 1627845600,client1,factorA,10
+ * 1627845605,client2,factorB,15
+ * 1627845610,client1,factorA,5
+ * 1627845610,client1,factorB,8
+ * 1627845620,client2,factorB,20
+ * 2
+ * factorA,5
+ * factorB,7
+ * 输出
+ * client1,131
+ * client2,245
+ */
+
+public class Main01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        HashSet<String> ses = new HashSet<>();
+        Map<String, Integer> h1 = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String s = scanner.next();
+            String[] a = s.split(",");
+            String ts = a[0] + a[1] + a[2];
+            if (ses.contains(ts)) {
+                continue;
+            }
+            ses.add(ts);
+            int tt = Integer.parseInt(a[3]);
+            if (tt < 0 || tt > 100) {
+                continue;
+            }
+            String key = a[1] + "_" + a[2];
+            h1.put(key, h1.getOrDefault(key, 0) + tt);
+        }
+
+        int m = scanner.nextInt();
+        Map<String, Integer> h2 = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            String f = scanner.next();
+            String[] b = f.split(",");
+            h2.put(b[0], Integer.parseInt(b[1]));
+        }
+
+        Map<String, Integer> h3 = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : h1.entrySet()) {
+            String[] keys = entry.getKey().split("_");
+            String k = keys[0];
+            int v = entry.getValue();
+            h3.put(k, h3.getOrDefault(k, 0) + h2.get(keys[1]) * v);
+        }
+
+        List<Map.Entry<String, Integer>> res = new ArrayList<>(h3.entrySet());
+        res.sort((o1, o2) -> o1.getKey().compareTo(o2.getKey()));
+
+        for (Map.Entry<String, Integer> entry : res) {
+            System.out.println(entry.getKey() + "," + entry.getValue());
+        }
+    }
+}
---
+++ leetcode/java/com/exam/huawei/round1/Main03.java
@@ -0,0 +1,88 @@
+package com.exam.huawei.round1;
+
+/**
+ * 3.网络保卫战
+ * 公有云的某个region内，N个网络节点组网情况可以使用一个n* n的矩阵matrix表示，在这个组网图中，matrix[i][j] = p 时，
+ * 表示用户在编号为 i的节点访问编号为j的节点时，必须在 i节点上具有>=p 的权限等级(p=0时表示无法通过第i节点访问j节点)，
+ * 如果用户成功访问了j节点，那么它在j节点上的权限等级调整为 P。
+ * exposed 为一个整数数组，表示暴露在公网上的网络节点的编号列表。
+ * 某天扫描发现这批暴需在公网的节点存在被外部恶意攻击风险且该攻击会影响到可访问的其他节点，并可以持续传递进行攻击，
+ * 被恶意攻击的节点从公网访问时，攻击者获得了ROOT 权限(权限等级为10，即最大值)。
+ * 小李是一名网络安全工程师，为了在有限的时间内尽可能的减少故障带来的损失，需要立即将某个节点从公网"下线"。
+ * 假设攻击结束时，被攻击过的节点数量为R，请帮小李计算出将哪个节点下线能使R尽可能小，如果答案有多个节点，返回索引最小的那个节点。
+ * 请注意：从公网“下线”的节点，不会受到来自公网的攻击，但仍然可能被“可访问”的其他节点传递攻击。
+ * 解答要求
+ * 时间限制: C/C++ 5000ms,其他语言: 10000ms内存限制: C/C++ 128MB，其他语言: 256MB
+ * 输入
+ * 输入的第一行是网络节点数量N
+ * 后续的N行，每行N个数字v，以空格分割，形成一个N*N的矩阵，表示网络节点组网的矩阵。
+ * 最后一行，输入 exposed 数组，表示暴露在公网上的网络节点的编号列表，数组元素不会重复。
+ * 输入范围说明：
+ * 2<=n<=24
+ * 0<=v<=10
+ * 0<=exposed[i]<=n-1
+ * 输出
+ * 输出在 exposed 数组中，计划"下线”的那个节点的编号。
+ * 样例1
+ * 输入
+ * 4
+ * 1 0 0 0
+ * 0 1 2 0
+ * 0 1 1 4
+ * 0 0 3 1
+ * 1 3
+ * 输出
+ * 3
+ */
+
+import java.util.Scanner;
+
+public class Main03 {
+    static int n;
+    static int[][] a = new int[30][30];
+    static boolean[] st = new boolean[30];
+
+    public static int dfs(int id, int ra) {
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            if (id == i || a[id][i] == 0) continue;
+            if (ra >= a[id][i]) {
+                if (st[i]) continue;
+                st[i] = true;
+                num += dfs(i, a[id][i]);
+                st[i] = false;
+            }
+        }
+
+        return num + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scanner.nextInt();
+            }
+        }
+
+        int x;
+        int res = -1;
+        int ans = 0;
+        while (scanner.hasNextInt()) {
+            x = scanner.nextInt();
+            for (int i = 0; i < n; i++) {
+                if (x == i) continue;
+                st[x] = true;
+                int t = dfs(x, 10);
+                st[x] = false;
+                if (t > ans) {
+                    ans = t;
+                    res = x;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+}
