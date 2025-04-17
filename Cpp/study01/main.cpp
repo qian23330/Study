5c23d35409a77368b09f6b8deac7043ef8d0dcba
qianlong <1339852932@qq.com>
2025-04-17 11:33:27 +0800

    25-04-17

--- Cpp/study01/main.cpp
+++ Cpp/study01/main.cpp
@@ -44,7 +44,43 @@ int main()
     cout << "a\tbbb" << endl;
 
     // 字符串
+    char str[] = "hello world";
+    cout << str << endl;
+    string str2 = "hello world";
+    cout << str2 << endl;
 
+    // 布尔类型
+    bool flag = true;
+    cout << flag << endl;
+    flag = false;
+    cout << flag << endl;
+    cout << sizeof(flag) << endl;
+
+    // 数据输入
+    int a = 100;
+    cout << a << endl;
+    cin >> a;
+    cout << a << endl;
+
+    float f = 3.14f;
+    cout << f << endl;
+    cin >> f;
+    cout << f << endl;
+
+    char chc = 'a';
+    cout << chc << endl;
+    cin >> chc;
+    cout << chc << endl;
+
+    string str3 = "hello C++";
+    cout << str3 << endl;
+    cin >> str3;
+    cout << str3 << endl;
+
+    bool flag2 = true;
+    cout << flag2 << endl;
+    cin >> flag2;
+    cout << flag2 << endl;
 
     return 0;
 }
