7c644fc217b3ddc1e1d9f65a95d00b93c8389335
qianlong <1339852932@qq.com>
2025-05-26 09:39:43 +0800

    25-05-26

--- Cpp/study04/main.cpp
+++ Cpp/study04/main.cpp
@@ -18,5 +18,12 @@ int main()
     {
         cout << "哈哈哈" << endl;
     }
+
+    // 三目运算符
+    int a = 10;
+    int b = 20;
+    int c = 0;
+    c = a > b ? a : b;
+    cout << "c的新值为：" << c << endl;
     return 0;
 }
